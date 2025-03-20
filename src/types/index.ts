91b2fd91944afd2e30a87639d17039db929b45b4
rapsag79 <servisechimino79@gmail.com>
2025-03-20 08:46:01 -0600

    primeros pasos

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,4 @@
 export type Category = {
-    id: number,
-    name: string
-}
-
-export type Activity = {
-    id: string
-    category: number
-    name: string
-    calories: number
-}
+  id: number;
+  name: string;
+};
